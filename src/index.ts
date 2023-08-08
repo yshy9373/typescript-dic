11ca1559f7b3d6a9495883e8e17fa1601a75e21a
Hee Yoon <idealicehee@gmail.com>
2023-08-08 16:16:28 +0900

    ✅ 메소드 이름 수정

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 type Words = {
+  // 해시
   [key: string]: string | string[];
+  //객체의 property에 대해 모르지만 타입만을 알 때 유용하다
 };
 class Dict {
   private words: Words;
@@ -13,11 +15,11 @@ class Dict {
       this.words[word.term] = word.def;
     }
   }
-  def(term: string) {
+  find(term: string) {
     return this.words[term];
   }
   // 단어를 삭제
-  delete(term: string) {
+  rmv(term: string) {
     delete this.words[term];
   }
   // 단어 이름 업데이트
@@ -37,16 +39,24 @@ class Dict {
       console.log(`${key}: ${value}`);
     }
   }
+  static hello() {
+    // 클래스를 생성 안해도 사용 가능
+    return 'hello';
+  }
 }
 // words는 initializer 없이 선언해주고 contructor에서 수동으로 초기화
 // constructor에 인자로 넣어 constructor가 지정해주길 바라는 게 아니므로
 
 // 각각의 단어에 대한 클래스
 class Word {
-  constructor(public term: string, public def: string | string[]) {}
+  constructor(
+    // 값을 보여주고는 싶지만 수정은 불가능하게
+    public readonly term: string,
+    public def: string | string[]
+  ) {}
   // 단어 출력하는 메소드
   toString() {
-    console.log(`${this.term}: ${this.def}`);
+    console.log(`${this.term}: [뜻] ${this.def}`);
   }
   // 단어 정의 추가
   addDef(newDef: string) {
@@ -66,3 +76,5 @@ class Word {
     }
   }
 }
+
+Dict.hello();
